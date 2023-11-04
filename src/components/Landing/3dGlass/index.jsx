9acb342c3f9e7079b7a93f3b10338b7d07977d2c
elson <elsonnezaj2005@gmail.com>
2023-11-04 19:16:00 +0100

    added auto rotation to 3d model

--- src/components/Landing/3dGlass/index.jsx
+++ src/components/Landing/3dGlass/index.jsx
@@ -1,9 +1,31 @@
 import { Canvas } from "@react-three/fiber";
 import { useGLTF, Stage, PresentationControls } from "@react-three/drei";
 import styles from "./styles.module.scss";
+import { useEffect } from "react";
+import * as THREE from "three";
 
 function Glasses(props) {
   const { scene } = useGLTF("/glasses.glb");
+  const camera = new THREE.PerspectiveCamera(
+    75,
+    window.innerWidth / window.innerHeight,
+    0.1,
+    1000
+  );
+  const renderer = new THREE.WebGLRenderer();
+
+  const animate = () => {
+    requestAnimationFrame(animate);
+    if (scene) {
+      // scene.rotation.x += 0.01;
+      scene.rotation.y += 0.01;
+    }
+    renderer.render(scene, camera);
+  };
+
+  useEffect(() => {
+    animate();
+  }, []);
   return <primitive object={scene} {...props} />;
 }
 
@@ -15,9 +37,9 @@ export default function ModelView() {
       camera={{ fov: 45 }}
       className={styles.glasses}
     >
-      <PresentationControls speed={1.5} polar={[-0.1, 4]}>
-        <Stage environment={"city"}>
-          <Glasses scale={0.07} />
+      <PresentationControls speed={1.5} polar={[-0.1, 1]}>
+        <Stage environment={"night"}>
+          <Glasses scale={0.045} />
         </Stage>
       </PresentationControls>
     </Canvas>
