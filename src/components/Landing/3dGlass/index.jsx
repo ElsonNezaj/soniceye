eb40c19fdee506fcee806edd47f21435d6087439
elson <elsonnezaj2005@gmail.com>
2023-11-25 12:49:16 +0100

    rendered 3d model for single item

--- src/components/Landing/3dGlass/index.jsx
+++ src/components/Landing/3dGlass/index.jsx
@@ -4,7 +4,8 @@ import styles from "./styles.module.scss";
 import { useEffect } from "react";
 import * as THREE from "three";
 
-function Glasses(props) {
+export function Glasses(props) {
+  const {isAnimated} = props;
   const camera = new THREE.PerspectiveCamera(
     75,
     window.innerWidth / window.innerHeight,
@@ -24,6 +25,7 @@ function Glasses(props) {
   };
 
   useEffect(() => {
+    isAnimated &&
     animate();
   });
   return <primitive object={scene} {...props} />;
@@ -38,7 +40,7 @@ export default function ModelView() {
       className={styles.glasses}
     >
       <Stage environment="night">
-        <Glasses scale={window.innerWidth > 500 ? 0.5 : 0.09} />
+        <Glasses isAnimated={true} scale={window.innerWidth > 500 ? 0.5 : 0.09} />
       </Stage>
     </Canvas>
   );
--- src/components/Products/ListProduct/index.jsx
+++ src/components/Products/ListProduct/index.jsx
@@ -2,10 +2,31 @@ import React from "react";
 import styles from "./styles.module.scss";
 import { Typography } from "antd";
 
+import { Glasses } from "../../Landing/3dGlass";
+import { Canvas } from "react-three-fiber";
+import { Stage } from "@react-three/drei";
+
 export default function ListProduct() {
   return (
     <div className={styles.listProductContainer}>
-      <Typography>LIST PRODUCT</Typography>
+      <div className={styles.modelContainer}>
+        <Canvas
+          dpr={[1, 2]}
+          shadows
+          camera={{ fov: 45 }}
+          className={styles.glasses}
+        >
+          <Stage environment="night">
+            <Glasses
+              isAnimated={false}
+              scale={window.innerWidth > 500 ? 0.9 : 0.09}
+            />
+          </Stage>
+        </Canvas>
+      </div>
+      <div className={styles.productDescription}>
+        <Typography className={styles.productName}>LIST PRODUCT</Typography>
+      </div>
     </div>
   );
 }
