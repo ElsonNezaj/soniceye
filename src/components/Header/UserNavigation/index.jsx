65d5278f0cb44bc112198154e1235cc5cf6d83b4
elson <elsonnezaj2005@gmail.com>
2024-01-01 20:07:39 +0100

    fixes for product row

--- src/components/Header/UserNavigation/index.jsx
+++ src/components/Header/UserNavigation/index.jsx
@@ -1,10 +1,12 @@
 import React from "react";
+import { useAppSelector } from "../../../redux/hooks";
 import styles from "./styles.module.scss";
 import { Typography } from "antd";
 import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
 import PersonIcon from "@mui/icons-material/Person";
 
 export default function UserNavigation() {
+  const cartItems = useAppSelector((state) => state.cart.cartItems);
   return (
     <div className={styles.userNavigationContainer}>
       <div className={styles.conectNumberContainer}>
@@ -12,6 +14,11 @@ export default function UserNavigation() {
         <Typography className={styles.label}>+355 069 123 4567</Typography>
       </div>
       <div className={styles.navigationContainer}>
+        <div className={styles.badgeContainer}>
+          <Typography className={styles.badgeLabel}>
+            {cartItems.length}
+          </Typography>
+        </div>
         <div className={styles.iconContainer}>
           <ShoppingCartIcon />
         </div>
