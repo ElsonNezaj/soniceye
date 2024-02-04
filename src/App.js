975b8fb15b033f709c4d65634989f2ed48455df9
elson <elsonnezaj2005@gmail.com>
2024-02-04 19:22:26 +0100

    getting data on app.js

--- src/App.js
+++ src/App.js
@@ -4,9 +4,19 @@ import Header from "./components/Header";
 import CircularProgress from "@mui/material/CircularProgress";
 import { Typography } from "antd";
 import { useAppSelector } from "./redux/hooks";
+import { useEffect } from "react";
+import { onValue, ref } from "firebase/database";
+import { db } from "./firebase";
 
 function App() {
   const isFetchingAuth = useAppSelector((state) => state.auth.isFetchingAuth);
+
+  useEffect(() => {
+    onValue(ref(db, "/orders"), (snapshot) => {
+      const data = snapshot.val();
+      console.log(data);
+    });
+  }, []);
   return (
     <div className="App">
       {isFetchingAuth && (
