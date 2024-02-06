8091ab1a08f345294875344112a38fa07efd121a
elson <elsonnezaj2005@gmail.com>
2024-02-06 16:10:05 +0100

    saving users on register to realtime database

--- src/App.js
+++ src/App.js
@@ -5,18 +5,38 @@ import CircularProgress from "@mui/material/CircularProgress";
 import { Typography } from "antd";
 import { useAppSelector } from "./redux/hooks";
 import { useEffect } from "react";
-import { onValue, ref } from "firebase/database";
+import { onValue, ref, set } from "firebase/database";
 import { db } from "./firebase";
 
 function App() {
   const isFetchingAuth = useAppSelector((state) => state.auth.isFetchingAuth);
+  const isAuth = useAppSelector((state) => state.auth.isAuth);
+  const authUser = useAppSelector((state) => state.auth.authUser);
 
   useEffect(() => {
-    onValue(ref(db, "/orders"), (snapshot) => {
-      const data = snapshot.val();
-      console.log(data);
-    });
-  }, []);
+    authUser &&
+      isAuth &&
+      onValue(ref(db, "/users"), (snapshot) => {
+        const data = snapshot.val();
+        if (data) {
+          const snapData = snapshot.val();
+          const keys = Object.keys(snapData);
+          const userExists = keys.find((el) => el === authUser.uid);
+          if (userExists) {
+            return;
+          } else {
+            set(ref(db, `/users/${authUser.uid}`), {
+              ...authUser,
+            });
+          }
+        } else {
+          set(ref(db, `/users/${authUser.uid}`), {
+            ...authUser,
+          });
+        }
+      });
+  }, [isAuth, authUser]);
+
   return (
     <div className="App">
       {isFetchingAuth && (
