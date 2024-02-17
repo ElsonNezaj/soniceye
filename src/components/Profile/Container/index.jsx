21dcf1366dda3f7ab72efd9db74a4ee381042264
elson <elsonnezaj2005@gmail.com>
2024-02-17 18:28:55 +0100

    changes for profile mobile and tablet

--- src/components/Profile/Container/index.jsx
+++ src/components/Profile/Container/index.jsx
@@ -1,20 +1,8 @@
-// import { signOutRequested } from "../../../redux/authSlice/authslice";
-// import { useAppDispatch, useAppSelector } from "../../../redux/hooks";
 import InputSide from "../InputSide";
 import ProfileSide from "../ProfileSide";
 import styles from "./styles.module.scss";
 
 export default function ProfileContainer() {
-  // const dispatch = useAppDispatch();
-  // const userAuth = useAppSelector((state) => state.auth.userAuth);
-  // const cartItems = useAppSelector((state) => state.cart.cartItems);
-  // const handleSignOut = () => {
-  //   cartItems.length >= 1
-  //     ? dispatch(signOutRequested({ uid: userAuth?.uid, items: cartItems }))
-  //     : dispatch(signOutRequested({ state: "no-update" }));
-  //   window.location.href = "/";
-  // };
-
   return (
     <div className={styles.profileContainer}>
       <div className={styles.leftSide}>
--- src/components/Profile/OrdersReview/index.jsx
+++
@@ -1,7 +0,0 @@
-import styles from "./styles.module.scss";
-
-export default function OrdersReview() {
-  return (
-    <div className={styles.ordersReviewContainer}>ORDERS REVIEW CONTAINER</div>
-  );
-}
