15a8bce54551cf40b10480619f93fcc91365728d
elson <elsonnezaj2005@gmail.com>
2024-02-20 15:50:13 +0100

    added inputs for profile edit

--- src/components/Profile/EditInputs/index.jsx
+++ src/components/Profile/EditInputs/index.jsx
@@ -1,6 +1,27 @@
 import styles from "./styles.module.scss";
-import { Typography, Button } from "antd";
+import { useAppSelector } from "../../../redux/hooks";
+import { Typography, Button, Form, Input } from "antd";
 
 export default function EditInputs() {
-  return <div className={styles.editInputsContainer}>EDIT INPUTS</div>;
+  const userAuth = useAppSelector((state) => state.auth.authUser);
+  return (
+    <div className={styles.editInputsContainer}>
+      <Typography className={styles.title}>Edit your profile</Typography>
+      <Typography className={styles.subTitle}>
+        Modify your personal data
+      </Typography>
+      <Form className={styles.editForm}>
+        <Input
+          type="text"
+          placeholder="Display Name"
+          className={styles.input}
+        />
+        <Input type="text" placeholder="E-mail" className={styles.input} />
+        <Input type="text" placeholder="Address" className={styles.input} />
+        <Input type="text" placeholder="City" className={styles.input} />
+        <Input type="text" placeholder="Country" className={styles.input} />
+        <Input type="text" placeholder="Zip Code" className={styles.input} />
+      </Form>
+    </div>
+  );
 }
